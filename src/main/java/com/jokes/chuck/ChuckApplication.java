68612f0375489684924c800fd1915ef9619d4df1
aliba <19654755+alice-ba@users.noreply.github.com>
2018-03-18 21:20:39 +0100

    view commit

--- src/main/java/com/jokes/chuck/ChuckApplication.java
+++ src/main/java/com/jokes/chuck/ChuckApplication.java
@@ -4,15 +4,15 @@ import com.jokes.chuck.controllers.JokesController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.ui.Model;
 
 @SpringBootApplication
 public class ChuckApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ac = SpringApplication.run(ChuckApplication.class, args);
-
 		JokesController controller = (JokesController) ac.getBean("jokesController");
-		System.out.println(controller.giveJokes());
+
 	}
 
 }
--- src/main/java/com/jokes/chuck/controllers/JokesController.java
+++ src/main/java/com/jokes/chuck/controllers/JokesController.java
@@ -1,8 +1,11 @@
 package com.jokes.chuck.controllers;
 
+
 import com.jokes.chuck.services.ReturnJokes;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
@@ -10,11 +13,17 @@ public class JokesController {
 
     private ReturnJokes returnJokes;
 
-    public JokesController(@Qualifier("returnJokesImpl") ReturnJokes returnJokes) {
+    @Autowired
+    public JokesController( ReturnJokes returnJokes) {
         this.returnJokes = returnJokes;
     }
 
-    public String giveJokes() {
-        return returnJokes.getJokes();
+
+    @RequestMapping( {"/",""})
+    public String getJokeText(Model model){
+
+        model.addAttribute("joke", returnJokes.getJokes());
+
+        return "chucknorris";
     }
 }
